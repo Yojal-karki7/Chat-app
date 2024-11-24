8bb72c1f90bb2f7c3195e3ebd08bc64c088828e7
Yojal-karki7 <yojalkarki@gmail.com>
2024-11-24 18:20:50 +0545

    get all messages

--- Server/index.js
+++ Server/index.js
@@ -6,6 +6,7 @@ import mongoose from "mongoose"
 import authRoutes from "./routes/AuthRoutes.js"
 import contactsRoutes from "./routes/contactRoutes.js"
 import setupSocket from "./socket.js"
+import messagesRoutes from "./routes/MessagesRoute.js"
 
 dotenv.config();
 
@@ -25,6 +26,7 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes)
 app.use("/api/contacts", contactsRoutes)
+app.use('/api/messages', messagesRoutes)
 
 
 const server = app.listen(port, ()=>{
